ecc7b543a83996f02b9e25a36e6ae37603197d94
Soham Kolhatkar <sohamk130405@gmail.com>
2025-08-26 19:04:48 +0530

    user-routes-added

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,22 @@
+// package imports
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+// config imports
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+
+// routes imports
+import userRoutes from "./routes/user.route.js";
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Hello from server✌️"));
 
