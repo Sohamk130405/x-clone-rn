99dbbd7afce59ac6533bb3171d906cdcb44b20aa
Soham Kolhatkar <sohamk130405@gmail.com>
2025-09-12 10:08:56 +0530

    create post functionality added

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,15 +1,27 @@
-import { Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SignOutButton from "@/components/signout";
 import { useUserSync } from "@/hooks/useUserSync";
+import { Ionicons } from "@expo/vector-icons";
+import PostComposer from "@/components/post-composer";
 
 const Home = () => {
   useUserSync();
   return (
-    <SafeAreaView className="flex-1">
-      <Text>Home</Text>
-      <SignOutButton />
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={24} color="#1da1f2" />
+        <Text className="text-xl font-bold text-gray-900">Home</Text>
+        <SignOutButton />
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={{ paddingBottom: 80 }}
+      >
+        <PostComposer />
+      </ScrollView>
     </SafeAreaView>
   );
 };
